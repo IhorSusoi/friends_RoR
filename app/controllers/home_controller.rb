46b35c89e618a4b2a9faa8da8f75709b1b1ccfec
Ihor Susoi <ihor.susoi@gmail.com>
2023-10-22 13:58:02 +0300

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
+        @ihorsus = "This is home/index. \nMy name is Ihor Susoi, and i know where appear zminni"
   end
 
   def about
+        @ihorsus = "My name is Ihor Susoi, and i know where appear zminni"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   #get 'home/index'
-  root 'home#index'
+  
+  #root 'home#index'
+
+  root 'friends#index'
+
   get 'home/about'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
